f5f4293aa9975d7480246d423a2a30759029fb1f
agusvalori <61027212+agusvalori@users.noreply.github.com>
2022-10-07 00:00:51 -0300

    'd'

--- server/src/config.js
+++ server/src/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
-const PORT_SERVER = process.env.PORT_SERVER || 4000;
+const PORT_SERVER = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGOATLAS_URI || "mongodb://localhost/articleDb";
 
 export { PORT_SERVER, MONGODB_URI };
