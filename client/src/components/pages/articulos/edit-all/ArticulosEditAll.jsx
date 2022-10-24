21d3e85f0cd4296b4d520e7849d53ca30c86a2bc
agusvalori <61027212+agusvalori@users.noreply.github.com>
2022-10-24 13:59:38 -0300

    Editando articuloEditAll filtrando articulos por categoria

--- client/src/components/pages/articulos/edit-all/ArticulosEditAll.jsx
+++ client/src/components/pages/articulos/edit-all/ArticulosEditAll.jsx
@@ -12,33 +12,70 @@ import { Box } from "@mui/system";
 import React, { useState } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import { useEffect } from "react";
+import { ArticuloTableVir } from "../table-virtualized/ArticuloTableVir";
 
 export const ArticulosEditAll = ({ articulos }) => {
   const [open, setOpen] = useState(false);
   const [categorias, setCategorias] = useState([]);
   const [subCategorias, setSubCategorias] = useState([]);
   const [proveedor, setProveedor] = useState([]);
+  const initialValuesSelect = {
+    PROVEEDOR: false,
+    CATEGORIA: false,
+    SUB_CATEGORIA: false,
+  };
+  const [valuesSelect, setValuesSelect] = useState(initialValuesSelect);
+  const [articulosFiltrados, setArticulosFiltrados] = useState(
+    articulos ? articulos : []
+  );
 
   const handleClose = () => {
     setOpen(false);
   };
 
+  const handleChangeSelect = (event) => {
+    const { name, value } = event.target;
+    setValuesSelect({ ...valuesSelect, [name]: value });
+  };
+
+  useEffect(() => {
+    console.log("Actualizando articulos filtrados");
+    setArticulosFiltrados(
+      articulos
+        .filter(
+          (item) =>
+            valuesSelect?.CATEGORIA &&
+            item?.CATEGORIA === valuesSelect?.CATEGORIA
+        )
+    );
+  }, [valuesSelect]);
+
   useEffect(() => {
     if (open) {
       setCategorias(
-        Array.from(new Set(articulos.map((item) => item.CATEGORIA)))
+        Array.from(
+          new Set(
+            articulos
+              ?.filter((item) => item?.CATEGORIA != undefined)
+              .map((item) => item.CATEGORIA)
+          )
+        ).sort()
       );
       setSubCategorias(
         Array.from(
-          new Set(articulos.map((item) => item?.SUB_CATEGORIA?.split(",")))
-        )
+          new Set(
+            articulos
+              ?.filter((item) => item?.SUB_CATEGORIA != undefined)
+              .map((item) => item?.SUB_CATEGORIA?.split(","))
+              .reduce((pre, cur) => pre.concat(cur))
+          )
+        ).sort()
       );
     }
-  }, [open, setCategorias]);
+  }, [open]);
 
   return (
     <Box>
-      {console.log("categorias: ", open, subCategorias)}
       <IconButton onClick={() => setOpen(!open)}>
         <EditIcon color="info" fontSize="large" />
       </IconButton>
@@ -50,30 +87,57 @@ export const ArticulosEditAll = ({ articulos }) => {
             <Box>
               <FormControl sx={{ width: "250px" }}>
                 <InputLabel>Categoria</InputLabel>
-                <Select>
-                  {categorias?.map((item) => (
-                    <MenuItem>{item}</MenuItem>
-                  ))}
+                <Select
+                  name="CATEGORIA"
+                  value={valuesSelect.CATEGORIA}
+                  onChange={handleChangeSelect}
+                >
+                  {categorias?.map(
+                    (item) =>
+                      item != undefined && (
+                        <MenuItem key={item} value={item}>
+                          {item}
+                        </MenuItem>
+                      )
+                  )}
                 </Select>
               </FormControl>
             </Box>
             <Box>
               <FormControl sx={{ width: "250px" }}>
-                <InputLabel>Subategoria</InputLabel>
-                <Select>
-                  {categorias?.map((item) => (
-                    <MenuItem>{item}</MenuItem>
-                  ))}
+                <InputLabel>Subcategoria</InputLabel>
+                <Select
+                  name="SUB_CATEGORIA"
+                  value={valuesSelect.SUB_CATEGORIA}
+                  onChange={handleChangeSelect}
+                >
+                  {subCategorias?.map(
+                    (item) =>
+                      item != undefined && (
+                        <MenuItem key={item} value={item}>
+                          {item}
+                        </MenuItem>
+                      )
+                  )}
                 </Select>
               </FormControl>
             </Box>
             <Box>
               <FormControl sx={{ width: "250px" }}>
                 <InputLabel>Proveedor</InputLabel>
-                <Select>
-                  {categorias?.map((item) => (
-                    <MenuItem>{item}</MenuItem>
-                  ))}
+                <Select
+                  name="PROVEEDOR"
+                  value={valuesSelect.PROVEEDOR}
+                  onChange={handleChangeSelect}
+                >
+                  {proveedor?.map(
+                    (item) =>
+                      item != undefined && (
+                        <MenuItem key={item} value={item}>
+                          {item}
+                        </MenuItem>
+                      )
+                  )}
                 </Select>
               </FormControl>
             </Box>
@@ -86,6 +150,9 @@ export const ArticulosEditAll = ({ articulos }) => {
             que vamos a editar: precios, restar o sumar un valor precios, restar
             o sumar un % Categorias: cambiar nombre acategorias Subcategoria:
             cambiar nombre subcategoria
+          </Box>
+          <Box>
+            <ArticuloTableVir articulos={articulosFiltrados} />
           </Box>
         </Paper>
       </Modal>
